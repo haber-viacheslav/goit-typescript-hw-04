89aa91cf10cbae3b3c2243d0ae0aaad165e1f7d0
Viacheslav Haber <gaberslavik@gmail.com>
2023-09-14 09:28:26 +0300

    added marks

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -5,18 +5,14 @@ type Props = {
 	children: ReactElement;
 	onContentEndVisible(): void;
 };
-type Options = {
-	rootMargin: string;
-	threshold: number;
-	root: HTMLElement | null;
-};
+
 export function Observer({ children, onContentEndVisible }: Props) {
 	// Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
 	const endContentRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		// Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-		const options: Options = {
+		const options: IntersectionObserverInit = {
 			rootMargin: "0px",
 			threshold: 1.0,
 			root: null,
